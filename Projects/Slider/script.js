1bd64ac3adb9370959441cef9a6fd6ed32899223
Vishesh-Kumar <19vishesh@gmail.com>
2022-12-08 08:36:00 +0530

    commit

---
+++ Projects/Slider/script.js
@@ -0,0 +1,16 @@
+const slides = document.querySelectorAll('.slide')
+var counter = 0;
+
+slides.forEach(
+    (slide,index) => { slide.style.left = `${index * 100}%`}
+)
+
+const slideImage = () => 
+{
+    slides.forEach (
+        (slide) => {slide.style.transform = `translateX(-${(counter%4)*100}%)`}
+    )
+}
+
+const goNext = () => { counter++; slideImage()}
+const goPrev = () => { counter--; slideImage()}
