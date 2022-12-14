5ecbfea0e64ad2c19ad6800f05958359473df947
Vishesh-Kumar <19vishesh@gmail.com>
2022-12-14 11:32:49 +0530

    commit

--- JavaScript/Tip-Calculator/script.js
+++ JavaScript/Tip-Calculator/script.js
@@ -22,7 +22,7 @@ These are all the DIV ID's you're gonna need access to 👇
 const billInput = document.getElementById('billTotalInput');
 const tipInput = document.getElementById('tipInput');
 const numberOfPeopleDiv = document.getElementById('numberOfPeople');
-const perPersonTotal = document.getElementById('perPersonTotal');
+const perPersonTotalDiv = document.getElementById('perPersonTotal');
 
 
 // Get number of people from number of people div
@@ -35,31 +35,34 @@ const calculateBill = () => {
     const bill = Number(billInput.value)
 
     // get the tip from user & convert it into a percentage (divide by 100)
-    
+    const tipPercentage = Number(tipInput.value) / 100;
 
     // get the total tip amount
-
+    const tipAmount = bill * tipPercentage
+    // console.log({tipAmount})
 
     // calculate the total (tip amount + bill)
-
+    const total = tipAmount + bill
+            // console.log({total})
 
     // calculate the per person total (total divided by number of people)
-
+    const perPersonTotal = total / numberOfPeople
 
     // update the perPersonTotal on DOM & show it to user
+    perPersonTotalDiv.innerText = `₹${perPersonTotal.toFixed(2).toLocaleString('en-IN')}`
 
 }
 
 // ** Splits the bill between more people **
 const increasePeople = () => {
     // increment the amount of people
-
+    numberOfPeople += 1
 
     // update the DOM with the new number of people
-
+    numberOfPeopleDiv.innerText = numberOfPeople
 
     // calculate the bill based on the new number of people
-
+    calculateBill()
 }
 
 // ** Splits the bill between fewer people **
@@ -67,14 +70,17 @@ const decreasePeople = () => {
     // guard clause
     // if amount is 1 or less simply return
     // (a.k.a you can't decrease the number of people to 0 or negative!)
-
+    if(numberOfPeople <= 1)
+        return
+        // we can also use throw instead of return
+        // throw 'you can't go below 1
 
     // decrement the amount of people
-
+    numberOfPeople -= 1
 
     // update the DOM with the new number of people
-
+    numberOfPeopleDiv.innerText = numberOfPeople
 
     // calculate the bill based on the new number of people
-
+    calculateBill()
 }
