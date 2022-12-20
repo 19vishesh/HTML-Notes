bcffe36aa9ed90b6afec0d6af48f679b31e35db8
Vishesh-Kumar <19vishesh@gmail.com>
2022-12-20 16:14:49 +0530

    coomit

---
+++ JavaScript/mini projects/pop-up/script.js
@@ -0,0 +1,8 @@
+let popbtn = document.getElementById('popup')
+
+function openPopup() {
+    popbtn.classList.add('open-popup')
+}
+function closePopup() {
+    popbtn.classList.remove('open-popup')
+}
